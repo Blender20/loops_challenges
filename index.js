e5d48f8a9a608be36f3fff924c5b07444786fb3e
Blender20 <rayyazdi@outlook.com>
2022-03-28 13:16:07 -0400

    javaScript loop challange

---
+++ index.js
@@ -0,0 +1,92 @@
+
+
+
+for (i = 1; i <20; i+=2){ // i is an odd number and while is less than 20 keeps adding 2 to it.
+  console.log(i)
+}
+
+for (i = 100; i >= 100; i--){if(i% 3 ==0) console.log(i)}
+
+// var multiplyByThree = num
+// if(num = parseInt(num/3)){
+// console.log(num == div / 3);
+// };
+// for (let i = 0; i <=100; i--){
+//   if(multiplyByThree(i)){
+//     console.log(i);
+//   }
+// }
+// var sum = 0;
+// for(i=0;i<=100; i++){
+//   if(i%3===0){
+//     console.log(sum.reverse(sum-=i)
+//   }
+// }
+// 5-
+// var res = "";
+// for(var i=0; i<10; i++){
+//   res += "**";
+//   console.log(res);
+// }
+
+// for(var i=0; i<3; i++) { // 0,1,2
+//   console.log(i);
+// }
+// for(var i=10; i>0; i--) { // 10,9,8,7,6,5,4,3,2,1
+//   console.log(i);
+// }
+// for(var i=12; i>3; i-=2) { // 12,10,8,6,4
+//   console.log(i);
+// }
+// for(var i=0.25; i<3; i+=0.5) { //0.25, 0.75, 1.25, 1.75, 2.25, 2.75
+//   console.log(i);
+// }
+// for(var i=0; i<3; i++) { //0,1,2
+//   console.log(i);
+// }
+  
+// var i = 0;
+// while(i<3) {
+//   console.log(i); //0,1,2
+//   i++;
+// }
+// var start = 0; 
+// var end = 10;
+    
+// while(start <= end) {
+//     console.log("start: " + start + ", end: " + end);
+//     start++;
+//     end--;
+// }
+ //start: 0, end: 10
+// start: 1, end: 9
+// start: 2, end: 8
+// start: 3, end: 7
+// start: 4, end: 6
+// start: 5, end: 5
+
+  for(var i=100; i>0; i--) { 
+      
+      if(i%3===0){console.log(i);}
+  }
+
+
+
+  for (i = 4; i>=-3.5; i-=1.5){
+  console.log(i);
+}
+
+
+var sum = 0;
+for (var i = 1; i<101; i++){
+  sum+=i;
+}
+console.log(sum)
+
+
+
+var multiple = 1;
+for (var i = 1; i<13; i++){
+  multiple*=i;
+}
+console.log(multiple)
